dec4d29e4f9f468129b22c9995f0e3d5ccc582f8
Istvan Nagy <istvan@istvan>
2017-07-11 15:03:47 +0200

    first commit

---
+++ src/test/kotlin/uy/com/collokia/pdfParserTest/PdfParserTest.kt
@@ -0,0 +1,16 @@
+package uy.com.collokia.pdfParserTest
+
+/**
+ * Created by istvan on 7/11/17.
+ */
+
+
+class PdfParserTest {
+
+    companion object {
+        fun main(args: Array<String>) {
+
+        }
+    }
+}
+
